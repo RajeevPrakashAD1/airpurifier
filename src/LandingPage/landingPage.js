2d632d5ef473434148b0e84ee8e6baa00738d412
RajeevPrakashAD1 <prakashrajeev115@gmail.com>
2021-08-27 19:26:11 +0530

    section 2 3 4 completed

--- src/LandingPage/landingPage.js
+++ src/LandingPage/landingPage.js
@@ -3,10 +3,13 @@ import { Container, Row, Col } from 'react-bootstrap';
 import './landingPage.css';
 
 //sdtyled components
-import { S2div, PreBookButton, H1, P1 } from './styledcomponent';
+import { S2div, PreBookButton, H1, P1, S5div } from './styledcomponent';
 
 //images
 import s2 from './images/s2.svg';
+import s3 from './images/s3.svg';
+import s4 from './images/s4.svg';
+import s5 from './images/s5.svg';
 
 const LandingPage = () => {
 	return (
@@ -38,11 +41,83 @@ const LandingPage = () => {
 							</Col>
 
 							<Col lg="6" md="6" className="Cols2textDiv">
-								<img src={s2} alt="o" height="700px" width="400px" />
+								<img src={s2} alt="o" className="img" />
 							</Col>
 						</Row>
 					</S2div>
 				</Container>
+			</section>
+			<section id="section3">
+				<contaniner fluid>
+					<S2div>
+						<Row>
+							<Col sm={{ order: 'first' }} md="6" className="Cols2textDiv" xs={{ order: 'last' }}>
+								<img src={s3} alt="o" className="img" />
+							</Col>
+							<Col lg="6" md="6" sm="12" className="Cols2textDiv">
+								<div className="s2textDiv">
+									<H1>Nature gives us everything we need</H1>
+									<P1>
+										Airoco is an innovative, natural product design to be just as effective at
+										cleaning the air you breathe without the environmental impact. Inspired by the
+										British countyrside, Airoco uses natural colours, textures and materials to
+										connect you with nature, whilist hidden within a wealth of features works hard
+										to keep your air clean.
+									</P1>
+									<PreBookButton> Pre-Book Now</PreBookButton>
+								</div>
+							</Col>
+						</Row>
+					</S2div>
+				</contaniner>
+			</section>
+			<section id="section4">
+				<contaniner fluid>
+					<S2div>
+						<Row>
+							<Col lg="6" md="6" sm="12" className="Cols2textDiv">
+								<div className="s2textDiv">
+									<H1>Is this the future of Air purifier?</H1>
+									<P1>
+										While it was just a TV show, that little speech at the beginning of the original
+										Star Trek show really did do a good job of capturing our feelings about space.
+										It is those feelings that drive our love of astronomy and our desire to learn
+										more and more about it.
+									</P1>
+									<PreBookButton> Pre-Book Now</PreBookButton>
+								</div>
+							</Col>
+							<Col lg="6" md="6" className="" style={{ display: 'flex', justifyContent: 'flex-end' }}>
+								<img src={s4} alt="o" width="90%" />
+							</Col>
+						</Row>
+					</S2div>
+				</contaniner>
+			</section>
+
+			<section id="section5">
+				<contaniner fluid>
+					<S5div>
+						<Row>
+							<Col sm={{ order: 'first' }} md="6" className="s5i" xs={{ order: 'last' }}>
+								<img src={s5} alt="o" className="img" />
+							</Col>
+							<Col
+								lg="6"
+								md="6"
+								sm="12"
+								className="s5c"
+								style={{ display: 'flex', justifyContent: 'flex-end' }}
+							>
+								<div className="s5t">
+									<H1> AIROCO</H1>
+									<P1>#Nursing The Home</P1>
+									<PreBookButton> Pre-Book Now</PreBookButton>
+								</div>
+							</Col>
+						</Row>
+					</S5div>
+				</contaniner>
 			</section>
 		</React.Fragment>
 	);
--- src/LandingPage/styledcomponent.js
+++ src/LandingPage/styledcomponent.js
@@ -1,46 +1,65 @@
 import styled from 'styled-components';
-import { Button } from 'react-bootstrap';
 
 export const S2div = styled.div `
-	background-color: red;
-	width: 100%;
-	hight: 400px;
-	&img {
-		height: 400px;
-		width: 400px;
-	}
+	padding: 40px 0px;
+
 	display: flex;
+	flex-direction: column;
 	align-items: center;
 	justify-content: center;
 
+	@media (max-width: 751px) {
+		flex-direction: column-reverse;
+	}
+
 	& .s2textDiv {
-		background-color: blue;
 		margin-left: 50px;
+		margin-right: 50px;
+		padding: 40px 0px;
+
+		@media (max-width: 751px) {
+			margin-left: 0px;
+			margin-right: 0px;
+			text-align: center;
+			padding: 20px 20px;
+		}
 	}
 
 	& .Cols2textDiv {
-		background-color: green;
 		display: flex;
 		justify-content: center;
 		align-items: center;
 	}
 `;
 
-export const PreBookButton = styled(Button)
-`
-    background-color: #425C56;
-    border-color: #425C56;
-    &: hover{
-        background-color: #35683e;
-        border-color: 35683e;
-        
+export const S5div = styled.div `
+	&.s5i {
+		bacground-color: yellow;
+	}
 
-        
-    }
+	&.s6c {
+		background-color: red;
+	}
+`;
 
+export const PreBookButton = styled.button `
+	background-color: #425c56;
+	border-color: #425c56;
+	padding: 5px 15px;
+	color: white;
+	border: none;
+	border-radius: 5px;
 
-    
+	&: hover {
+		background-color: #35683e !important;
+		border-color: #35683e;
+	}
 
+	&:focus {
+		outline: none;
+		box-shadow: none;
+		shadow: none;
+	}
 `;
 
 export const H1 = styled.p `
@@ -53,6 +72,11 @@ export const H1 = styled.p `
 	/* primary */
 
 	color: #425c56;
+
+	@media (max-width: 500px) {
+		font-size: 20px;
+		line-height: 26px;
+	}
 `;
 
 export const P1 = styled.p `
@@ -65,4 +89,8 @@ export const P1 = styled.p `
 	/* primary */
 
 	color: #425c56;
+	@media (max-width: 500px) {
+		font-size: 12px;
+		line-height: 20px;
+	}
 `;
