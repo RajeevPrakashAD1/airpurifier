962793c03364cb85418cdfa56ed3f6d74c268477
RajeevPrakashAD1 <prakashrajeev115@gmail.com>
2021-09-01 21:17:54 +0530

    checking aws

--- src/LandingPage/landingPage.js
+++ src/LandingPage/landingPage.js
@@ -112,7 +112,7 @@ const LandingPage = () => {
 				<section id="section1">
 					<div className="tdiv">
 						<div>
-							<h1 className="title">AIR </h1>
+							<h1 className="title">Rajeev</h1>
 						</div>
 						<div>
 							<h1 className="title">OCO</h1>
